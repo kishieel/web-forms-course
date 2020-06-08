1fabdd7f0ac0fed559839e1999df078df8435396
TomaszKisiel <tkisiel5w4@yahoo.com>
2020-06-08 16:18:41 +0200

    Repaired not reloading code pens.

--- src/components/lessons/FirstLesson.js
+++ src/components/lessons/FirstLesson.js
@@ -6,19 +6,20 @@ const FirstLesson = () => {
       <h3 className="uk-card-title">Czym są formularze?</h3>
       <p>Lorem ipsum <a href="#">dolor</a> sit amasd sad sad asd asd sa dsad sadsad kaler asete laso a masnoa srpsa alsldsa asdlopsad ad asdsbta das saryja dsadet, conasd awd sectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <p>Lorem ipsum a masnoa srpsa alsldsa asdlopsad ad asdsbta das saryja dsadet, conasd awd sectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <p class="codepen"
-        data-height="300"
-        data-theme-id="dark"
-        data-default-tab="html,result"
-        data-user="tomaszkisiel"
-        data-slug-hash="gOPOmXm"
-        style={{ height: 300, boxSizing: 'border-box', display: 'flex', alignItems: 'center', justifyContent: 'center', border: '2px solid', margin: '1em', padding: '1em'}}>
-        <span>
-          See the Pen <a href="https://codepen.io/tomaszkisiel/pen/gOPOmXm">
-          web-forms-example-1</a> by TomaszKisiel (<a href="https://codepen.io/tomaszkisiel">@tomaszkisiel</a>)
-          on <a href="https://codepen.io">CodePen</a>.
-        </span>
-      </p>
+      <iframe
+        height="300"
+        scrolling="no"
+        title="web-forms-example-1"
+        src="https://codepen.io/tomaszkisiel/embed/gOPOmXm?height=265&theme-id=dark&default-tab=html,result"
+        frameborder="no"
+        allowtransparency="true"
+        allowfullscreen="true"
+        style={{ width: '100%' }}>
+        <div style={{ height: 300, boxSizing: 'border-box', display: 'flex', alignItems: 'center', justifyContent: 'center', border: '2px solid', margin: '1em', padding: '1em' }}>
+          See the Pen <a href='https://codepen.io/tomaszkisiel/pen/gOPOmXm'>web-forms-example-1</a> by TomaszKisiel
+          (<a href='https://codepen.io/tomaszkisiel'>@tomaszkisiel</a>) on <a href='https://codepen.io'>CodePen</a>.
+        </div>
+      </iframe>
       <p>Lorem ipsum a masnoa srpsa alsldsa asdlopsad ad asdsbta das saryja dsadet, conasd awd sectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </article>
   </>)
--- src/components/lessons/SecondLesson.js
+++ src/components/lessons/SecondLesson.js
@@ -4,6 +4,20 @@ const SecondLesson = () => {
   return (<>
     <article className="uk-card uk-card-default uk-card-body">
       <p>Lekcja druga</p>
+      <iframe
+        height="300"
+        scrolling="no"
+        title="web-forms-example-1"
+        src="https://codepen.io/tomaszkisiel/embed/gOPOmXm?height=265&theme-id=dark&default-tab=html,result"
+        frameborder="no"
+        allowtransparency="true"
+        allowfullscreen="true"
+        style={{ width: '100%' }}>
+        <div style={{ height: 300, boxSizing: 'border-box', display: 'flex', alignItems: 'center', justifyContent: 'center', border: '2px solid', margin: '1em', padding: '1em' }}>
+          See the Pen <a href='https://codepen.io/tomaszkisiel/pen/gOPOmXm'>web-forms-example-1</a> by TomaszKisiel
+          (<a href='https://codepen.io/tomaszkisiel'>@tomaszkisiel</a>) on <a href='https://codepen.io'>CodePen</a>.
+        </div>
+      </iframe>
     </article>
   </>)
 }
